859287fc2acbd958a1981680abf1190465d973d1
MegaStick87 <66920071+MegaStick87@users.noreply.github.com>
2024-03-03 23:50:30 -0600

    Add files via upload

---
+++ script_menu.js
@@ -0,0 +1,5 @@
+        /* How are you seeing this? Umm, but yes, I did this to prevent you from right clicking. */
+        document.addEventListener("contextmenu", function(e){
+            e.preventDefault();
+          }, false);
+
